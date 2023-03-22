50dddd50b18b878d843dcec0957f455688d44009
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-22 10:13:12 +0200

    Modify to use socketio for real time chat

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 import webbrowser
 
 app = Flask(__name__)
+app.config['SECRET'] = "secret!123"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     # Open the app in the default web browser
     webbrowser.open('http://localhost:5000')
-    
     # Start the Flask development server
-    app.run(debug=True)
+    socketio.run(app, host="localhost")
